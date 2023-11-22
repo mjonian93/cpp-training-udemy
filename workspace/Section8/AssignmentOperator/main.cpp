0f5f5da377ae99257170db531b46b89de8cd2f9d
Sainz Markel <Msainz@ikerlan.es>
2023-11-22 09:45:39 +0100

    Progress. Section7 finished.

---
+++ workspace/Section8/AssignmentOperator/main.cpp
@@ -0,0 +1,20 @@
+//Section 8
+//Assignment
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1 {10};
+    int num2 {20};
+    
+    num1 = num2;
+    
+    cout << "num1 is " << num1 << endl;
+    cout << "num2 is " << num2 << endl;
+    
+    cout << endl;
+    return 0;
+}
