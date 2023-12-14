0e9fbc8eb8f2b2688e4fb637611f26bfb1379266
Markel Sainz <msainz@ikerlan.es>
2023-12-14 14:34:24 +0100

    Course finished

--- workspace/Section21/STL_Lambdas/main.cpp
+++ workspace/Section21/STL_Lambdas/main.cpp
@@ -275,3 +275,15 @@ void test7() {
         
     std::cout << std::endl;
 }
+
+int main() {
+    test1();
+    test2();
+    test3();
+    test4();
+    test5();
+    test6();
+    test7();
+
+    return 0;
+}
---
+++ workspace/Section4/CompilerErrors/CompilerErrors.mk
@@ -0,0 +1,107 @@
+##
+## Auto Generated makefile by CodeLite IDE
+## any manual changes will be erased      
+##
+## Debug
+ProjectName            :=CompilerErrors
+ConfigurationName      :=Debug
+WorkspaceConfiguration :=Debug
+WorkspacePath          :=/home/msainz/Training/cpp-training-udemy/workspace/Section4
+ProjectPath            :=/home/msainz/Training/cpp-training-udemy/workspace/Section4/CompilerErrors
+IntermediateDirectory  :=../build-$(WorkspaceConfiguration)/CompilerErrors
+OutDir                 :=$(IntermediateDirectory)
+CurrentFileName        :=
+CurrentFilePath        :=
+CurrentFileFullPath    :=
+User                   :=
+Date                   :=11/20/23
+CodeLitePath           :=/home/msainz/.codelite
+MakeDirCommand         :=mkdir -p
+LinkerName             :=g++
+SharedObjectLinkerName :=g++ -shared -fPIC
+ObjectSuffix           :=.o
+DependSuffix           :=.o.d
+PreprocessSuffix       :=.o.i
+DebugSwitch            :=-gstab
+IncludeSwitch          :=-I
+LibrarySwitch          :=-l
+OutputSwitch           :=-o 
+LibraryPathSwitch      :=-L
+PreprocessorSwitch     :=-D
+SourceSwitch           :=-c 
+OutputDirectory        :=/home/msainz/Training/cpp-training-udemy/workspace/Section4/build-$(WorkspaceConfiguration)/bin
+OutputFile             :=../build-$(WorkspaceConfiguration)/bin/$(ProjectName)
+Preprocessors          :=
+ObjectSwitch           :=-o 
+ArchiveOutputSwitch    := 
+PreprocessOnlySwitch   :=-E 
+ObjectsFileList        :=$(IntermediateDirectory)/ObjectsList.txt
+PCHCompileFlags        :=
+LinkOptions            :=  -O0
+IncludePath            :=  $(IncludeSwitch). $(IncludeSwitch). 
+IncludePCH             := 
+RcIncludePath          := 
+Libs                   := 
+ArLibs                 :=  
+LibPath                := $(LibraryPathSwitch). $(LibraryPathSwitch). $(LibraryPathSwitch)Debug 
+
+##
+## Common variables
+## AR, CXX, CC, AS, CXXFLAGS and CFLAGS can be overridden using an environment variable
+##
+AR       := ar rcus
+CXX      := g++
+CC       := gcc
+CXXFLAGS :=  -g -Wall $(Preprocessors)
+CFLAGS   :=   $(Preprocessors)
+ASFLAGS  := 
+AS       := as
+
+
+##
+## User defined environment variables
+##
+CodeLiteDir:=/usr/share/codelite
+Objects0=$(IntermediateDirectory)/main.cpp$(ObjectSuffix) 
+
+
+
+Objects=$(Objects0) 
+
+##
+## Main Build Targets 
+##
+.PHONY: all clean PreBuild PrePreBuild PostBuild MakeIntermediateDirs
+all: MakeIntermediateDirs $(OutputFile)
+
+$(OutputFile): $(IntermediateDirectory)/.d $(Objects) 
+	@$(MakeDirCommand) "$(IntermediateDirectory)"
+	@echo "" > $(IntermediateDirectory)/.d
+	@echo $(Objects0)  > $(ObjectsFileList)
+	$(LinkerName) $(OutputSwitch)$(OutputFile) @$(ObjectsFileList) $(LibPath) $(Libs) $(LinkOptions)
+
+MakeIntermediateDirs:
+	@$(MakeDirCommand) "$(IntermediateDirectory)"
+	@$(MakeDirCommand) "$(OutputDirectory)"
+
+$(IntermediateDirectory)/.d:
+	@$(MakeDirCommand) "$(IntermediateDirectory)"
+
+PreBuild:
+
+
+##
+## Objects
+##
+$(IntermediateDirectory)/main.cpp$(ObjectSuffix): main.cpp 
+	$(CXX) $(IncludePCH) $(SourceSwitch) "/home/msainz/Training/cpp-training-udemy/workspace/Section4/CompilerErrors/main.cpp" $(CXXFLAGS) $(ObjectSwitch)$(IntermediateDirectory)/main.cpp$(ObjectSuffix) $(IncludePath)
+$(IntermediateDirectory)/main.cpp$(PreprocessSuffix): main.cpp
+	$(CXX) $(CXXFLAGS) $(IncludePCH) $(IncludePath) $(PreprocessOnlySwitch) $(OutputSwitch) $(IntermediateDirectory)/main.cpp$(PreprocessSuffix) main.cpp
+
+##
+## Clean
+##
+clean:
+	$(RM) -r $(IntermediateDirectory)
+
+
