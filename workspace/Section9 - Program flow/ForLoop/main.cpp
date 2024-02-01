855fdf8016af18acd7dabca9d8daab5e5d952aa3
mjonian93 <61407868+mjonian93@users.noreply.github.com>
2024-02-01 11:53:12 +0100

    remove unnecessary comments

--- workspace/Section9 - Program flow/ForLoop/main.cpp
+++ workspace/Section9 - Program flow/ForLoop/main.cpp
@@ -8,32 +8,32 @@ using namespace std;
 
 int main() {
     
-//    for (int i{1}; i<=10; i++)
-//        cout << i << endl;
-//
-//    for (int i{1}; i<=10; i+=2)
-//        cout << i << endl;
-//
-//    for (int i{10}; i>0; i--)
-//        cout << i << endl;
-//    
-//    cout << "Blastoff" << endl;
-//    
-//    for (int i{10}; i<=100; i+=10) {
-//        if (i % 15 == 0)
-//            cout << i << endl;
-//    }
-//    
-//    for (int i{1}, j{5}; i<=5; ++i, ++j) 
-//        cout << i << " + " << j << " = " << (i+j) << endl;
-//        
-//    for (int i{1}; i<=100; i++) {
-//        cout << i;
-//        if (i % 10 == 0)
-//            cout << endl;
-//        else
-//            cout << " ";
-//    }
+    for (int i{1}; i<=10; i++)
+        cout << i << endl;
+
+    for (int i{1}; i<=10; i+=2)
+        cout << i << endl;
+
+    for (int i{10}; i>0; i--)
+        cout << i << endl;
+    
+    cout << "Blastoff" << endl;
+    
+    for (int i{10}; i<=100; i+=10) {
+        if (i % 15 == 0)
+            cout << i << endl;
+    }
+    
+    for (int i{1}, j{5}; i<=5; ++i, ++j) 
+        cout << i << " + " << j << " = " << (i+j) << endl;
+        
+    for (int i{1}; i<=100; i++) {
+        cout << i;
+        if (i % 10 == 0)
+            cout << endl;
+        else
+            cout << " ";
+    }
     
     for (int i{1}; i<=100; i++) {
         cout << i << ((i % 10 == 0) ? "\n ": " ");
